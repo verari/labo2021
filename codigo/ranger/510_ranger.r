9886d2c081443a908b9bfa7e157c99d33e98eb90
AraneoA <37629991+AraneoA@users.noreply.github.com>
2021-09-17 17:18:15 -0300

    Add files via upload

---
+++ codigo/ranger/510_ranger.r
@@ -0,0 +1,60 @@
+#Se utiliza el algoritmo Random Forest, creado por Leo Breiman en el año 2001
+#Una libreria que implementa Rando Forest se llama  ranger
+#La libreria esta implementada en lenguaje C y corre en paralelo, utiliza TODOS los nucleos del procesador
+#Leo Breiman provenia de la estadistica y tenia "horror a los nulos", con lo cual el algoritmo necesita imputar nulos antes
+
+
+#limpio la memoria
+rm( list=ls() )  #Borro todos los objetos
+gc()   #Garbage Collection
+
+require("data.table")
+require("ranger")
+require("randomForest")  #solo se usa para imputar nulos
+
+#Aqui se debe poner la carpeta de la computadora local
+setwd("~/buckets/b1/crudo/")  #Establezco el Working Directory
+
+#cargo los datos donde entreno
+dtrain  <- fread("./datasetsOri/paquete_premium_202011.csv", stringsAsFactors= TRUE)
+
+dtrain[ , clase_binaria := as.factor(ifelse( clase_ternaria=="BAJA+2", "POS", "NEG" )) ]
+dtrain[ , clase_ternaria := NULL ]  #elimino la clase_ternaria, ya no la necesito
+
+#imputo los nulos, ya que ranger no acepta nulos
+#Leo Breiman, ¿por que le temias a los nulos?
+dtrain  <- na.roughfix( dtrain )
+
+#cargo los datos donde aplico el modelo
+dapply  <- fread("./datasetsOri/paquete_premium_202101.csv", stringsAsFactors= TRUE)
+dapply[ , clase_ternaria := NULL ]  #Elimino esta columna que esta toda en NA
+dapply  <- na.roughfix( dapply )
+
+#genero el modelo de Random Forest con la libreria ranger
+param  <- list( "num.trees"=      500,  #cantidad de arboles
+                "mtry"=             sqrt(ncol(dtrain)),  #cantidad de variables que evalua para hacer un split
+                "min.node.size"=    1,  #hoja mas chica
+                "max.depth"=        0   # 0 significa profundidad infinita
+              )
+
+set.seed(102191) #Establezco la semilla aleatoria
+
+modelo  <- ranger( formula= "clase_binaria ~ .",
+                   data=  dtrain, 
+                   probability=   TRUE,  #para que devuelva las probabilidades
+                   num.trees=     param$num.trees,
+                   mtry=          param$mtry,
+                   min.node.size= param$min.node.size,
+                   max.depth=     param$max.depth
+                 )
+
+prediccion  <- predict( modelo, dapply )
+
+#Genero la entrega para Kaggle
+entrega  <- as.data.table( list( "numero_de_cliente"= dapply[  , numero_de_cliente],
+                                 "Predicted"= as.numeric(prediccion$predictions[ ,"POS" ] > 0.025) ) ) #genero la salida
+
+#genero el archivo para Kaggle
+fwrite( entrega, 
+        file="./kaggle/ranger_001.csv", 
+        sep="," )
---
+++ codigo/ranger/560_ranger_BO.r
@@ -0,0 +1,257 @@
+#Optimizacion Bayesiana de hiperparametros de  ranger
+#funciona automaticamente con EXPERIMENTOS
+#va generando incrementalmente salidas para kaggle
+
+#limpio la memoria
+rm( list=ls() )  #remove all objects
+gc()             #garbage collection
+
+require("data.table")
+require("rlist")
+require("yaml")
+
+require("ranger")
+require("randomForest")  #solo se usa para imputar nulos
+require("parallel")
+
+#paquetes necesarios para la Bayesian Optimization
+require("DiceKriging")
+require("mlrMBO")
+
+
+#defino la carpeta donde trabajo
+setwd( "~/buckets/b1/crudo/"  )
+
+
+kexperimento  <- NA   #NA si se corre la primera vez, un valor concreto si es para continuar procesando
+
+kscript           <- "560_ranger_BO"
+karch_generacion  <- "./datasetsOri/paquete_premium_202011.csv"
+karch_aplicacion  <- "./datasetsOri/paquete_premium_202101.csv"
+kBO_iter    <-  150   #cantidad de iteraciones de la Optimizacion Bayesiana
+
+hs  <- makeParamSet(
+          makeIntegerParam("num.trees" ,        lower=  2L  , upper=  500L),  #la letra L al final significa ENTERO
+          makeIntegerParam("max.depth",         lower=  0L  , upper=   20L),  # 0 significa profundidad infinita
+          makeIntegerParam("min.node.size" ,    lower=  1L  , upper=  500L),
+          makeIntegerParam("mtry" ,             lower=  2L  , upper=   50L))
+
+ksemilla_azar  <- 102191  #Aqui poner la propia semilla
+#------------------------------------------------------------------------------
+#Funcion que lleva el registro de los experimentos
+
+get_experimento  <- function()
+{
+  if( !file.exists( "./maestro.yaml" ) )  cat( file="./maestro.yaml", "experimento: 1000" )
+
+  exp  <- read_yaml( "./maestro.yaml" )
+  experimento_actual  <- exp$experimento
+
+  exp$experimento  <- as.integer(exp$experimento + 1)
+  Sys.chmod( "./maestro.yaml", mode = "0644", use_umask = TRUE)
+  write_yaml( exp, "./maestro.yaml" )
+  Sys.chmod( "./maestro.yaml", mode = "0444", use_umask = TRUE) #dejo el archivo readonly
+
+  return( experimento_actual )
+}
+#------------------------------------------------------------------------------
+#graba a un archivo los componentes de lista
+#para el primer registro, escribe antes los titulos
+
+loguear  <- function( reg, arch=NA, folder="./work/", ext=".txt", verbose=TRUE )
+{
+  archivo  <- arch
+  if( is.na(arch) )  archivo  <- paste0(  folder, substitute( reg), ext )
+
+  if( !file.exists( archivo ) )  #Escribo los titulos
+  {
+    linea  <- paste0( "fecha\t", 
+                      paste( list.names(reg), collapse="\t" ), "\n" )
+
+    cat( linea, file=archivo )
+  }
+
+  linea  <- paste0( format(Sys.time(), "%Y%m%d %H%M%S"),  "\t",     #la fecha y hora
+                    gsub( ", ", "\t", toString( reg ) ),  "\n" )
+
+  cat( linea, file=archivo, append=TRUE )  #grabo al archivo
+
+  if( verbose )  cat( linea )   #imprimo por pantalla
+}
+#------------------------------------------------------------------------------
+#funcion para particionar, es la que Andres reemplaza con caret
+
+particionar  <- function( data, division, agrupa="", campo="fold", start=1, seed=NA )
+{
+  if( !is.na( seed)  )   set.seed( seed )
+
+  bloque  <- unlist( mapply(  function(x,y) { rep( y, x ) }, division, seq( from=start, length.out=length(division) )  ) )
+
+  data[ , (campo) :=  sample( rep( bloque, ceiling(.N/length(bloque))) )[1:.N],
+           by= agrupa ]
+}
+#------------------------------------------------------------------------------
+
+ranger_Simple  <- function( fold_test, pdata, param )
+{
+  #genero el modelo
+
+  set.seed(ksemilla_azar)
+
+  modelo  <- ranger( formula= "clase_binaria ~ .",
+                     data=  pdata[ fold!= fold_test], 
+                     probability=   TRUE,  #para que devuelva las probabilidades
+                     num.trees=     param$num.trees,
+                     mtry=          param$mtry,
+                     min.node.size= param$min.node.size,
+                     max.depth=     param$max.depth
+                 )
+
+  prediccion  <- predict( modelo, pdata[ fold==fold_test] )
+
+  ganancia_testing  <- pdata[ fold==fold_test,
+                              sum( (prediccion$predictions[ ,"POS" ] > 0.025) *
+                                    ifelse( clase_binaria=="POS", 48750, -1250)  ) ]
+
+  return( ganancia_testing )
+}
+#------------------------------------------------------------------------------
+
+ranger_CrossValidation  <- function( data, param, pcampos_buenos, qfolds, pagrupa, semilla )
+{
+  divi  <- rep( 1, qfolds )
+  particionar( data, divi, seed=semilla, agrupa=pagrupa )
+
+  ganancias  <- mcmapply( ranger_Simple, 
+                          seq(qfolds), # 1 2 3 4 5  
+                          MoreArgs= list( data, param), 
+                          SIMPLIFY= FALSE,
+                          mc.cores= 1 )   #dejar esto en  1, porque ranger ya corre en paralelo
+
+  data[ , fold := NULL ]
+  #devuelvo la primer ganancia y el promedio
+  return( mean( unlist( ganancias )) *  qfolds )   #aqui normalizo la ganancia
+}
+#------------------------------------------------------------------------------
+#esta funcion solo puede recibir los parametros que se estan optimizando
+#el resto de los parametros se pasan como variables globales
+
+EstimarGanancia_ranger  <- function( x )
+{
+   GLOBAL_iteracion  <<-  GLOBAL_iteracion + 1
+
+   xval_folds  <- 5
+   ganancia  <- ranger_CrossValidation( dataset, 
+                                        param= x,
+                                        qfolds= xval_folds, 
+                                        pagrupa= "clase_binaria", 
+                                        semilla= ksemilla_azar )
+
+   #si tengo una ganancia superadora, genero el archivo para Kaggle
+   if(  ganancia > GLOBAL_ganancia_max )
+   {
+     GLOBAL_ganancia_max  <<- ganancia  #asigno la nueva maxima ganancia
+
+     set.seed(ksemilla_azar)
+
+     modelo  <- ranger( formula= "clase_binaria ~ .",
+                        data=  dataset, 
+                        probability=   TRUE,  #para que devuelva las probabilidades
+                        num.trees=     x$num.trees,
+                        mtry=          x$mtry,
+                        min.node.size= x$min.node.size,
+                        max.depth=     x$max.depth
+                      )
+
+     prediccion  <- predict( modelo, dapply )
+
+     Predicted  <- as.integer( prediccion$predictions[ ,"POS" ] > 0.025 )
+
+     entrega  <- as.data.table( list( "numero_de_cliente"=dapply$numero_de_cliente, 
+                                      "Predicted"= Predicted)  )
+
+     #genero el archivo para Kaggle
+     fwrite( entrega, 
+             file= paste0(kkaggle, GLOBAL_iteracion, ".csv" ),
+             sep=  "," )
+   }
+
+   #logueo 
+   xx  <- x
+   xx$xval_folds  <-  xval_folds
+   xx$ganancia  <- ganancia
+   loguear( xx,  arch= klog )
+
+
+   return( ganancia )
+}
+#------------------------------------------------------------------------------
+#Aqui empieza el programa
+
+if( is.na(kexperimento ) )   kexperimento <- get_experimento()  #creo el experimento
+
+#en estos archivos quedan los resultados
+kbayesiana  <- paste0("./work/E",  kexperimento, "_rpart.RDATA" )
+klog        <- paste0("./work/E",  kexperimento, "_rpart_log.txt" )
+kkaggle     <- paste0("./kaggle/E",kexperimento, "_", kscript, "_" )
+
+
+GLOBAL_ganancia_max  <-  -Inf
+GLOBAL_iteracion  <- 0
+
+if( file.exists(klog) )
+{
+ tabla_log  <- fread( klog)
+ GLOBAL_iteracion  <- nrow( tabla_log ) -1
+ GLOBAL_ganancia_max  <-  tabla_log[ , max(ganancia) ]
+}
+
+
+#cargo el datset donde voy a entrenar
+dataset  <- fread(karch_generacion, stringsAsFactors= TRUE)   #donde entreno
+
+dataset[ , clase_binaria := as.factor(ifelse( clase_ternaria=="BAJA+2", "POS", "NEG" )) ]
+dataset[ , clase_ternaria := NULL ]  #elimino la clase_ternaria, ya no la necesito
+#imputo los nulos, ya que ranger no acepta nulos
+#Leo Breiman, ¿por que le temias a los nulos?
+dataset  <- na.roughfix( dataset )
+
+
+#cargo el dataset donde voy a aplicar el modelo, que NO tiene clase
+dapply   <- fread(karch_aplicacion, stringsAsFactors= TRUE)   #donde aplico el modelo
+dapply[ , clase_ternaria := NULL ]  #Elimino esta columna que esta toda en NA
+dapply  <- na.roughfix( dapply )
+
+
+
+#Aqui comienza la configuracion de la Bayesian Optimization
+
+configureMlr( show.learner.output = FALSE)
+
+funcion_optimizar  <- EstimarGanancia_ranger
+
+#configuro la busqueda bayesiana,  los hiperparametros que se van a optimizar
+#por favor, no desesperarse por lo complejo
+obj.fun  <- makeSingleObjectiveFunction(
+              fn=       funcion_optimizar,
+              minimize= FALSE,   #estoy Maximizando la ganancia
+              noisy=    TRUE,
+              par.set=  hs,
+              has.simple.signature = FALSE
+             )
+
+ctrl  <- makeMBOControl( save.on.disk.at.time= 600,  save.file.path= kbayesiana)
+ctrl  <- setMBOControlTermination(ctrl, iters= kBO_iter )
+ctrl  <- setMBOControlInfill(ctrl, crit= makeMBOInfillCritEI())
+
+surr.km  <-  makeLearner("regr.km", predict.type= "se", covtype= "matern3_2", control= list(trace= TRUE))
+
+#inicio la optimizacion bayesiana
+if(!file.exists(kbayesiana)) {
+  run  <- mbo(obj.fun, learner = surr.km, control = ctrl)
+} else  run  <- mboContinue( kbayesiana )   #retomo en caso que ya exista
+
+
+quit( save="no" )
+
+
